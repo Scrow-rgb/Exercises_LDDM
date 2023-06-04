b2968ca0e3f8e0485e76843be07ce0c7059c2a5a
Caio Silva <78487065+Scrow-rgb@users.noreply.github.com>
2023-06-04 14:36:33 -0300

    Update tela_cadastro.dart

--- quarto_app/lib/tela_cadastro.dart
+++ quarto_app/lib/tela_cadastro.dart
@@ -1,83 +1,148 @@
 import 'package:flutter/material.dart';
-import 'tela_cadastro.dart';
-
-void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login Screen',
+      title: 'Cadastro',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const LoginPage(),
-        '/register': (context) => RegisterPage(),
-      },
+      home: const RegisterPage(),
     );
   }
 }
 
-class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  bool _passwordVisible = false;
+  bool _isMale = true;
+  bool _emailNotifications = false;
+  bool _phoneNotifications = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Login'),
+        title: const Text('Cadastro'),
       ),
-      body: Container(
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Container(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Nome',
               ),
-              const SizedBox(height: 12.0),
-              const TextField(
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                ),
-                obscureText: true,
+              maxLength: 50,
+            ),
+            const SizedBox(height: 12.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Data de Nascimento',
               ),
-              const SizedBox(height: 24.0),
-              TextButton(
-                onPressed: () {
-                  // Lógica de autenticação
-                },
-                child: const Text('Enter'),
+              keyboardType: TextInputType.datetime,
+            ),
+            const SizedBox(height: 12.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'Telefone',
               ),
-              const SizedBox(height: 12.0),
-              GestureDetector(
-                onTap: () {
-                  Navigator.pushNamed(context, '/register');
-                },
-                child: const Text(
-                  'New here? Create an account',
-                  style: TextStyle(
-                    color: Colors.blue,
-                    decoration: TextDecoration.underline,
+              keyboardType: TextInputType.phone,
+              maxLength: 15,
+            ),
+            const SizedBox(height: 12.0),
+            const TextField(
+              decoration: InputDecoration(
+                labelText: 'E-mail',
+              ),
+              keyboardType: TextInputType.emailAddress,
+            ),
+            const SizedBox(height: 12.0),
+            TextField(
+              decoration: InputDecoration(
+                labelText: 'Senha',
+                suffixIcon: IconButton(
+                  icon: Icon(
+                    _passwordVisible ? Icons.visibility_off : Icons.visibility,
                   ),
+                  onPressed: () {
+                    setState(() {
+                      _passwordVisible = !_passwordVisible;
+                    });
+                  },
                 ),
               ),
-            ],
-          ),
+              obscureText: !_passwordVisible,
+            ),
+            const SizedBox(height: 12.0),
+            Row(
+              children: [
+                Checkbox(
+                  value: _isMale,
+                  onChanged: (value) {
+                    setState(() {
+                      _isMale = value!;
+                    });
+                  },
+                ),
+                const Text('Masculino'),
+                const SizedBox(width: 16.0),
+                Checkbox(
+                  value: !_isMale,
+                  onChanged: (value) {
+                    setState(() {
+                      _isMale = !value!;
+                    });
+                  },
+                ),
+                const Text('Feminino'),
+              ],
+            ),
+            const SizedBox(height: 12.0),
+            Row(
+              children: [
+                Switch(
+                  value: _emailNotifications,
+                  onChanged: (value) {
+                    setState(() {
+                      _emailNotifications = value;
+                    });
+                  },
+                ),
+                const Text('Notificações via E-mail'),
+              ],
+            ),
+            const SizedBox(height: 12.0),
+            Row(
+              children: [
+                Switch(
+                  value: _phoneNotifications,
+                  onChanged: (value) {
+                    setState(() {
+                      _phoneNotifications = value;
+                    });
+                  },
+                ),
+                const Text('Notificações via Telefone'),
+              ],
+            ),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: () {
+                // Lógica para cadastrar o usuário
+              },
+              child: const Text('Cadastrar'),
+            ),
+          ],
         ),
       ),
     );
